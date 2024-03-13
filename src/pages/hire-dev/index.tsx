89c8d28b2f63584a1c44870f8f1ea79463f39497
anup <anupdineshtandale@gmail.com>
2024-03-13 16:45:31 +0530

    b

--- src/pages/hire-dev/index.tsx
+++ src/pages/hire-dev/index.tsx
@@ -19,24 +19,123 @@ import { Button } from "@mui/material";
 import { useState } from "react";
 const Hiredev = () => {
     const [profileButton, setProfileButton] = useState("sap");
-  const router = useRouter();
-  const HireDeveloperClicked = () => {
-    // router.push("/search-developers");
-  };
-  const WatchVideoClicked = () => {
-    alert("WatchVideoClicked Clicked");
-  };
-  const handleClick = () => {
-    // router.push('/search-developers');
-  }
-  return (
-    <div>
-      <section>
-        <Navbar />
-      </section>
-      
-    </div>
-  )
+    const router = useRouter();
+    const HireDeveloperClicked = () => {
+        // router.push("/search-developers");
+    };
+    const WatchVideoClicked = () => {
+        alert("WatchVideoClicked Clicked");
+    };
+    const handleClick = () => {
+        // router.push('/search-developers');
+    }
+    return (
+        <div>
+            <section>
+                <Navbar />
+            </section>
+            <div className={hireDevStyles.wholeContainer}>
+                <br />
+                <section className={hireDevStyles.hireDevTopSection} >
+                    <div className={hireDevStyles.flexCenter2}>
+                        <div >
+                            <div className={`${hireDevStyles.mainHeadingHireDev}`}>
+                                {HireDeveloperPageText.HireDeveloperHeroHeading}
+                            </div>
+
+                            <h2 className={`${hireDevStyles.mainSubHeadingHireDev}`}>
+                                {HireDeveloperPageText.HireDeveloperHeroPara}
+                            </h2>
+                            <br />
+                            <div style={{ display: "flex", gap: "15px" }}>
+                                <CustomButton2
+                                    label={HireDeveloperPageText.HireDeveloperHeroButton1}
+                                    onClick={HireDeveloperClicked}
+                                    buttonStyle={{
+                                        color: "white",
+                                        borderRadius: "5px",
+                                        background: "#424241",
+                                        fontSize: "18px",
+                                        padding: "10px 25px",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        gap: "10px",
+                                        margin: "0px 15px 0px 30px",
+                                        boxShadow: "5px 5px 35px 0px rgba(0, 0, 0, 0.25)",
+                                    }}
+                                    hoverStyle={{
+                                        color: "black",
+                                        borderRadius: "5px",
+                                        background: "white",
+                                        fontSize: "18px",
+                                        padding: "10px 25px",
+                                        border: "1px solid black",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        gap: "10px",
+                                        margin: "0px 15px 0px 30px",
+                                        boxShadow: "5px 5px 35px 0px rgba(0, 0, 0, 0.25)",
+                                    }}
+                                />
+                                <br />
+                                <br />
+                                <IconTextButton
+                                    icon={<PlayCircleFilledWhiteOutlinedIcon />}
+                                    text="Watch Video"
+                                    onClick={WatchVideoClicked}
+                                />
+                            </div>
+                            <br />
+                            <br />
+                            <br />
+
+
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "start",
+                                    gap: "50px",
+                                    margin: "0px 0px 0px 20px",
+                                }}
+                            >
+                                <div>
+                                    <div className={`${hireDevStyles.hireDevNumberTitle}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum1}
+                                    </div>
+                                    <span className={`${hireDevStyles.hireDevNumberContent}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum1Title}
+                                    </span>
+                                </div>
+                                <div>
+                                    <div className={`${hireDevStyles.hireDevNumberTitle}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum2}
+                                    </div>
+                                    <span className={`${hireDevStyles.hireDevNumberContent}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum2Title}
+                                    </span>
+                                </div>
+                                <div>
+                                    <div className={`${hireDevStyles.hireDevNumberTitle}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum3}
+                                    </div>
+                                    <span className={`${hireDevStyles.hireDevNumberContent}`}>
+                                        {HireDeveloperPageText.HireDeveloperHeroNum3Title}
+                                    </span>
+                                </div>
+
+                                <br />
+                            </div>
+                        </div>
+
+                        <div>
+                            {/* <ResponsiveCarousel opacityValue={1} skillSetValue="sap" /> */}
+                            <Image src="/HireDeveloperImages/hiring3.png" alt="" height={300} width={400} />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
 }
 
 export default Hiredev
