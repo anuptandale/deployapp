830add01cefe663de400b17917d62f03a4b5ccd4
anup <anupdineshtandale@gmail.com>
2024-03-20 13:24:22 +0530

    a

--- src/pages/sap-talent-pool/index.tsx
+++ src/pages/sap-talent-pool/index.tsx
@@ -93,50 +93,50 @@
             //         </div>
             //     </div>
             // </section>
-//             <section id={css.secondsectiongrey}>
-//                 <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>TECHNOLOGY + TALENT + KNOW-HOW</div>
-//                 <div style={{ fontSize: "18px", padding: "0px 200px", marginBottom: "50px", opacity: "0.7" }}>Our high-end, on-demand talent marketplace powers greater agility, innovation, outcomes, and competitive advantage. Through proprietary technology, vast talent knowledge, and skilled in-house consultants, we deliver a handpicked selection of freelance consultants and experts vetted for the project at hand.</div>
-//                 <div id={css.threeimage}>
-//                     <div id={css.threeimagesection}>
-//                         <div><Image src="/TalentPoolImages/service9.webp" height={130} width={130} alt="" /></div>
-//                         <div style={{ fontWeight: "600" }}>SHARE YOUR NEED</div>
-//                         <div style={{ fontSize: "14px" }}>Call, post, or chat details of your project, ideal consultant, budget, and timeline.</div>
-//                     </div>
-//                     <div id={css.threeimagesection}>
-//                         <div><Image src="/TalentPoolImages/service10.webp" height={130} width={130} alt="" /></div>
-//                         <div style={{ fontWeight: "600" }}>SELECT YOUR TALENT</div>
-//                         <div style={{ fontSize: "14px" }}>Choose your consultant or team of experts from a curated slate of talent.</div>
-//                     </div>
-//                     <div id={css.threeimagesection}>
-//                         <div><Image src="/TalentPoolImages/service11.webp" height={130} width={130} alt="" /></div>
-//                         <div style={{ fontWeight: "600" }}>GET THE WORK DONE</div>
-//                         <div style={{ fontSize: "14px" }}>Rest easy as your consultant is rapidly deployed onsite or remote. Let us take care of the rest.</div>
-//                     </div>
-//                 </div>
-//             </section>
-//             <section id={css.secondsection}>
-//                 <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>MARKETPLACE TALENT FACTS</div>
-//                 <div id={css.threeimage}>
-//                     <div id={css.threeimagesection}>
-//                         {/* <div style={{ fontWeight: "600" }}>CLASSICALLY TRAINED</div> */}
-//                         <div id={css.greenNumber}>53%</div>
+            // <section id={css.secondsectiongrey}>
+            //     <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>TECHNOLOGY + TALENT + KNOW-HOW</div>
+            //     <div style={{ fontSize: "18px", padding: "0px 200px", marginBottom: "50px", opacity: "0.7" }}>Our high-end, on-demand talent marketplace powers greater agility, innovation, outcomes, and competitive advantage. Through proprietary technology, vast talent knowledge, and skilled in-house consultants, we deliver a handpicked selection of freelance consultants and experts vetted for the project at hand.</div>
+            //     <div id={css.threeimage}>
+            //         <div id={css.threeimagesection}>
+            //             <div><Image src="/TalentPoolImages/service9.webp" height={130} width={130} alt="" /></div>
+            //             <div style={{ fontWeight: "600" }}>SHARE YOUR NEED</div>
+            //             <div style={{ fontSize: "14px" }}>Call, post, or chat details of your project, ideal consultant, budget, and timeline.</div>
+            //         </div>
+            //         <div id={css.threeimagesection}>
+            //             <div><Image src="/TalentPoolImages/service10.webp" height={130} width={130} alt="" /></div>
+            //             <div style={{ fontWeight: "600" }}>SELECT YOUR TALENT</div>
+            //             <div style={{ fontSize: "14px" }}>Choose your consultant or team of experts from a curated slate of talent.</div>
+            //         </div>
+            //         <div id={css.threeimagesection}>
+            //             <div><Image src="/TalentPoolImages/service11.webp" height={130} width={130} alt="" /></div>
+            //             <div style={{ fontWeight: "600" }}>GET THE WORK DONE</div>
+            //             <div style={{ fontSize: "14px" }}>Rest easy as your consultant is rapidly deployed onsite or remote. Let us take care of the rest.</div>
+            //         </div>
+            //     </div>
+            // </section>
+            // <section id={css.secondsection}>
+            //     <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>MARKETPLACE TALENT FACTS</div>
+            //     <div id={css.threeimage}>
+            //         <div id={css.threeimagesection}>
+            //             {/* <div style={{ fontWeight: "600" }}>CLASSICALLY TRAINED</div> */}
+            //             <div id={css.greenNumber}>53%</div>
 
-//                         <div style={{ fontSize: "14px", opacity: "0.8" }}>of the Fortune 100 served</div>
-//                     </div>
-//                     <div id={css.threeimagesection}>
-//                         {/* <div style={{ fontWeight: "600" }}>PROVEN & HANDS-ON</div> */}
-//                         <div id={css.greenNumber}>99%</div>
+            //             <div style={{ fontSize: "14px", opacity: "0.8" }}>of the Fortune 100 served</div>
+            //         </div>
+            //         <div id={css.threeimagesection}>
+            //             {/* <div style={{ fontWeight: "600" }}>PROVEN & HANDS-ON</div> */}
+            //             <div id={css.greenNumber}>99%</div>
 
-//                         <div style={{ fontSize: "14px", opacity: "0.8" }}>fill rate on all talent requests</div>
-//                     </div>
-//                     <div id={css.threeimagesection}>
-//                         {/* <div style={{ fontWeight: "600" }}>EXECUTION-ORIENTED</div> */}
-//                         <div id={css.greenNumber}>91%</div>
+            //             <div style={{ fontSize: "14px", opacity: "0.8" }}>fill rate on all talent requests</div>
+            //         </div>
+            //         <div id={css.threeimagesection}>
+            //             {/* <div style={{ fontWeight: "600" }}>EXECUTION-ORIENTED</div> */}
+            //             <div id={css.greenNumber}>91%</div>
 
-//                         <div style={{ fontSize: "14px", opacity: "0.8" }}>of clients explore a new project within 12 months</div>
-//                     </div>
-//                 </div>
-//             </section>
+            //             <div style={{ fontSize: "14px", opacity: "0.8" }}>of clients explore a new project within 12 months</div>
+            //         </div>
+            //     </div>
+            // </section>
 //             <section>
 //                 <Letstalk />
 //             </section>
@@ -197,17 +197,17 @@ const SapTalentPool = () => {
                     <div id={css.threeimagesection}>
                         <div><Image src="/TalentPoolImages/service6.webp" height={130} width={130} alt="" /></div>
                         <div style={{ fontWeight: "600" }}>TOP TALENT</div>
-                        <div style={{ fontSize: "14px" }}>Access independent consultants trained by Big 3 and boutique firms, plus experts and executives with experience at the world&apos;s most successful companies.</div>
+                        <div style={{ fontSize: "14px",width:"300px" }}>Access independent consultants trained by Big 3 and boutique firms, plus experts and executives with experience at the world&apos;s most successful companies.</div>
                     </div>
                     <div id={css.threeimagesection}>
                         <div><Image src="/TalentPoolImages/service7.webp" height={130} width={130} alt="" /></div>
                         <div style={{ fontWeight: "600" }}>WORLD-CLASS SERVICE</div>
-                        <div style={{ fontSize: "14px" }}>Get the service level you need—from hands-on support to automated and instant—to scope the work, present a curated slate of candidates, and manage all contracting and compliance.</div>
+                        <div style={{ fontSize: "14px",width:"300px" }}>Get the service level you need—from hands-on support to automated and instant—to scope the work, present a curated slate of candidates, and manage all contracting and compliance.</div>
                     </div>
                     <div id={css.threeimagesection}>
                         <div><Image src="/TalentPoolImages/service8.webp" height={130} width={130} alt="" /></div>
                         <div style={{ fontWeight: "600" }}>BUILT FOR BUSINESS</div>
-                        <div style={{ fontSize: "14px" }}>Rest easy with best-in-class information security, confidentiality, labor compliance, and contracting processes that adhere to the strict requirements of leading organizations.</div>
+                        <div style={{ fontSize: "14px",width:"300px" }}>Rest easy with best-in-class information security, confidentiality, labor compliance, and contracting processes that adhere to the strict requirements of leading organizations.</div>
                     </div>
                 </div>
             </section>
@@ -241,50 +241,24 @@ const SapTalentPool = () => {
                     </div>
                 </div>
             </section>
-            <section id={css.allsapOuterMost}>
-                <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>IN-DEMAND PROBLEM SOLVERS FOR ALL OF YOUR TRANSFORMATION CONSULTING NEEDS</div>
-                <div id={css.allsapOuter}>
-                    <div id={css.allsapInner}>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Financial Accounting (FI)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Controlling (CO)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; General Ledger (FI-GL)</div>
-
-
-
-                    </div>
-                    <div id={css.allsapInner}>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Accounts Payable (FI-AP)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Accounts Receivable (FI-AR)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Asset Accounting (FI-AA)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Profitability Analysis (CO-PA)</div>
-
-
-                    </div>
-                    <div id={css.allsapInner}>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Bank Accounting (FI-BL)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Cost Element Accounting (CO-OM-CEL)</div>
-                        <div ><FiberManualRecordIcon sx={{ fontSize: "15px", color: "grey" }} />&nbsp; Cost Center Accounting (CO-OM-CCA)</div>
-
-                    </div>
-                </div>
-            </section>
-            <section id={css.secondsection2}>
-                <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>LIMITLESS ACCESS TO TOP TRANSFORMATION CONSULTANTS</div>
-                <div id={css.threeimageprofile}>
+            <section id={css.secondsectiongrey}>
+                <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>TECHNOLOGY + TALENT + KNOW-HOW</div>
+                <div style={{ fontSize: "18px", padding: "0px 200px", marginBottom: "50px", opacity: "0.7" }}>Our high-end, on-demand talent marketplace powers greater agility, innovation, outcomes, and competitive advantage. Through proprietary technology, vast talent knowledge, and skilled in-house consultants, we deliver a handpicked selection of freelance consultants and experts vetted for the project at hand.</div>
+                <div id={css.threeimage}>
                     <div id={css.threeimagesection}>
-                        <Image style={{ borderRadius: "100px" }} src="/TalentPoolImages/person.jpg" height={130} width={130} alt="" />
-                        <div style={{ fontWeight: "600" }}>ALEX PASHKOWSKY</div>
-                        <div>Program Manager</div>
+                        <div><Image src="/TalentPoolImages/service9.webp" height={130} width={130} alt="" /></div>
+                        <div style={{ fontWeight: "600" }}>SHARE YOUR NEED</div>
+                        <div style={{ fontSize: "14px" }}>Call, post, or chat details of your project, ideal consultant, budget, and timeline.</div>
                     </div>
                     <div id={css.threeimagesection}>
-                        <Image style={{ borderRadius: "100px" }} src="/TalentPoolImages/person.jpg" height={130} width={130} alt="" />
-                        <div style={{ fontWeight: "600" }}>ALEX PASHKOWSKY</div>
-                        <div>Program Manager</div>
+                        <div><Image src="/TalentPoolImages/service10.webp" height={130} width={130} alt="" /></div>
+                        <div style={{ fontWeight: "600" }}>SELECT YOUR TALENT</div>
+                        <div style={{ fontSize: "14px" }}>Choose your consultant or team of experts from a curated slate of talent.</div>
                     </div>
                     <div id={css.threeimagesection}>
-                        <Image style={{ borderRadius: "100px" }} src="/TalentPoolImages/person.jpg" height={130} width={130} alt="" />
-                        <div style={{ fontWeight: "600" }}>ALEX PASHKOWSKY</div>
-                        <div>Program Manager</div>
+                        <div><Image src="/TalentPoolImages/service11.webp" height={130} width={130} alt="" /></div>
+                        <div style={{ fontWeight: "600" }}>GET THE WORK DONE</div>
+                        <div style={{ fontSize: "14px" }}>Rest easy as your consultant is rapidly deployed onsite or remote. Let us take care of the rest.</div>
                     </div>
                 </div>
             </section>
@@ -292,46 +266,27 @@ const SapTalentPool = () => {
                 <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>MARKETPLACE TALENT FACTS</div>
                 <div id={css.threeimage}>
                     <div id={css.threeimagesection}>
-                        <div style={{ fontWeight: "600" }}>CLASSICALLY TRAINED</div>
-                        <div id={css.greenNumber}>8,100+</div>
+                        {/* <div style={{ fontWeight: "600" }}>CLASSICALLY TRAINED</div> */}
+                        <div id={css.greenNumber}>53%</div>
 
-                        <div style={{ fontSize: "14px", opacity: "0.8" }}>have worked at top consulting firms</div>
+                        <div style={{ fontSize: "14px", opacity: "0.8" }}>of the Fortune 100 served</div>
                     </div>
                     <div id={css.threeimagesection}>
-                        <div style={{ fontWeight: "600" }}>PROVEN & HANDS-ON</div>
-                        <div id={css.greenNumber}>64%</div>
+                        {/* <div style={{ fontWeight: "600" }}>PROVEN & HANDS-ON</div> */}
+                        <div id={css.greenNumber}>99%</div>
 
-                        <div style={{ fontSize: "14px", opacity: "0.8" }}>have both consulting and operational experience</div>
+                        <div style={{ fontSize: "14px", opacity: "0.8" }}>fill rate on all talent requests</div>
                     </div>
                     <div id={css.threeimagesection}>
-                        <div style={{ fontWeight: "600" }}>EXECUTION-ORIENTED</div>
-                        <div id={css.greenNumber}>5,600</div>
+                        {/* <div style={{ fontWeight: "600" }}>EXECUTION-ORIENTED</div> */}
+                        <div id={css.greenNumber}>91%</div>
 
-                        <div style={{ fontSize: "14px", opacity: "0.8" }}>experienced project and program managers</div>
+                        <div style={{ fontSize: "14px", opacity: "0.8"}}>of clients explore a new project within 12 months</div>
                     </div>
                 </div>
             </section>
-            <section id={css.insightsection}>
-                <div style={{ fontSize: "23px", fontWeight: "600", marginBottom: "50px" }}>TRANSFORMATION CONSULTING CASE STUDIES AND FEATURED INSIGHTS</div>
-                <div id={css.insightOuter}>
-                    <div id={css.insight}>
-                        <div style={{ fontWeight: "600" }}>The power of pace in technology</div>
-                        <div>Hivemind Technologies explains what steps matter in speeding software development for transformations.</div>
-                    </div>
-                    <div id={css.insight}>
-                        <div style={{ fontWeight: "600" }}>The power of pace in technology</div>
-                        <div>Hivemind Technologies explains what steps matter in speeding software development for transformations.</div>
-                    </div>
-                    <div id={css.insight}>
-                        <div style={{ fontWeight: "600" }}>The power of pace in technology</div>
-                        <div>Hivemind Technologies explains what steps matter in speeding software development for transformations.</div>
-                    </div>
-                    <div id={css.insight}>
-                        <div style={{ fontWeight: "600" }}>The power of pace in technology</div>
-                        <div>Hivemind Technologies explains what steps matter in speeding software development for transformations.</div>
-                    </div>
-                </div>
-            </section>
+            
+            
             <section>
                 <Letstalk />
             </section>
